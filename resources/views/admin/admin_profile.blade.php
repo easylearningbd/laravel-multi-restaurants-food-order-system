e24f78f65aa3291c47b9ce2c8aed25c5931e5801
easylearningbd <elebdsoft@gmail.com>
2024-05-14 04:50:16 +0600

    24 Admin Profile & Image Update Part 2

--- resources/views/admin/admin_profile.blade.php
+++ resources/views/admin/admin_profile.blade.php
@@ -56,6 +56,63 @@
                 </div>
                 <!-- end card -->
 
+
+<div class="card-body p-4">
+<form action="">
+<div class="row">
+    <div class="col-lg-6">
+        <div>
+            <div class="mb-3">
+                <label for="example-text-input" class="form-label">Name</label>
+                <input class="form-control" type="text" name="name" value="{{ $profileData->name }}" id="example-text-input">
+            </div>
+
+            <div class="mb-3">
+                <label for="example-text-input" class="form-label">Email</label>
+                <input class="form-control" name="email" type="email" value="{{ $profileData->email }}" id="example-text-input">
+            </div>
+
+            <div class="mb-3">
+                <label for="example-text-input" class="form-label">Phone</label>
+                <input class="form-control" name="phone" type="text" value="{{ $profileData->phone }}" id="example-text-input">
+            </div>
+              
+        </div>
+    </div>
+
+    <div class="col-lg-6">
+        <div class="mt-3 mt-lg-0">
+            <div class="mb-3">
+                <label for="example-text-input" class="form-label">Address</label>
+                <input class="form-control" name="address" type="text" value="{{ $profileData->address }}" id="example-text-input">
+            </div>
+
+            <div class="mb-3">
+                <label for="example-text-input" class="form-label">Profile Image</label>
+                <input class="form-control" name="photo" type="file"  id="image">
+            </div>
+            <div class="mb-3">
+                 
+                <img src="{{ (!empty($profileData->photo)) ? url('upload/admin_images/'.$profileData->photo) : url('upload/no_image.jpg') }}" alt="" class="rounded-circle p-1 bg-primary" width="110">
+            </div>
+            <div class="mt-4">
+                <button type="submit" class="btn btn-primary waves-effect waves-light">Save Changes</button>
+            </div>
+              
+        </div>
+    </div>
+</div>
+</form>
+</div>
+
+
+
+
+
+
+
+
+
                
                 <!-- end tab content -->
             </div>
