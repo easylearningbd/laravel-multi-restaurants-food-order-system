a1332cbda2cda6a7ad8bd19fb3a5d0d320391182
easylearningbd <elebdsoft@gmail.com>
2024-05-18 19:16:50 +0600

    39 Frontend Template Setup Part 2

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -8,16 +8,16 @@
           <span class="line"></span>
        </div>
        <div class="row">
-          <div class="col-md-12">
-             <div class="owl-carousel owl-carousel-four owl-theme">
-                <div class="item">
+
+          <div class="col-md-3">  
+                <div class="item pb-3">
                    <div class="list-card bg-white h-100 rounded overflow-hidden position-relative shadow-sm">
                       <div class="list-card-image">
                          <div class="star position-absolute"><span class="badge badge-success"><i class="icofont-star"></i> 3.1 (300+)</span></div>
                          <div class="favourite-heart text-danger position-absolute"><a href="detail.html"><i class="icofont-heart"></i></a></div>
                          <div class="member-plan position-absolute"><span class="badge badge-dark">Promoted</span></div>
                          <a href="detail.html">
-                         <img src="img/list/1.png" class="img-fluid item-img">
+                         <img src="{{ asset('frontend/img/list/1.png') }}" class="img-fluid item-img">
                          </a>
                       </div>
                       <div class="p-3 position-relative">
@@ -31,103 +31,147 @@
                          </div>
                       </div>
                    </div>
-                </div>
-                <div class="item">
-                   <div class="list-card bg-white h-100 rounded overflow-hidden position-relative shadow-sm">
-                      <div class="list-card-image">
-                         <div class="star position-absolute"><span class="badge badge-warning"><i class="icofont-star"></i> 3.1 (300+)</span></div>
-                         <div class="favourite-heart text-danger position-absolute"><a href="detail.html"><i class="icofont-heart"></i></a></div>
-                         <div class="member-plan position-absolute"><span class="badge badge-dark">Promoted</span></div>
-                         <a href="detail.html">
-                         <img src="img/list/3.png" class="img-fluid item-img">
-                         </a>
-                      </div>
-                      <div class="p-3 position-relative">
-                         <div class="list-card-body">
-                            <h6 class="mb-1"><a href="detail.html" class="text-black">Bite Me Sandwiches</a></h6>
-                            <p class="text-gray mb-3">North Indian • Indian • Pure veg</p>
-                            <p class="text-gray mb-3 time"><span class="bg-light text-dark rounded-sm pl-2 pb-1 pt-1 pr-2"><i class="icofont-wall-clock"></i> 15–25 min</span> <span class="float-right text-black-50"> $100 FOR TWO</span></p>
-                         </div>
-                         <div class="list-card-badge">
-                            <span class="badge badge-danger">OFFER</span> <small>65% off | Use Coupon OSAHAN50</small>
-                         </div>
-                      </div>
-                   </div>
-                </div>
-                <div class="item">
-                   <div class="list-card bg-white h-100 rounded overflow-hidden position-relative shadow-sm">
-                      <div class="list-card-image">
-                         <div class="star position-absolute"><span class="badge badge-success"><i class="icofont-star"></i> 3.1 (300+)</span></div>
-                         <div class="favourite-heart text-danger position-absolute"><a href="detail.html"><i class="icofont-heart"></i></a></div>
-                         <div class="member-plan position-absolute"><span class="badge badge-danger">Promoted</span></div>
-                         <a href="detail.html">
-                         <img src="img/list/6.png" class="img-fluid item-img">
-                         </a>
-                      </div>
-                      <div class="p-3 position-relative">
-                         <div class="list-card-body">
-                            <h6 class="mb-1"><a href="detail.html" class="text-black">The osahan Restaurant
-                               </a>
-                            </h6>
-                            <p class="text-gray mb-3">North • Hamburgers • Pure veg</p>
-                            <p class="text-gray mb-3 time"><span class="bg-light text-dark rounded-sm pl-2 pb-1 pt-1 pr-2"><i class="icofont-wall-clock"></i> 15–25 min</span> <span class="float-right text-black-50"> $500 FOR TWO</span></p>
-                         </div>
-                         <div class="list-card-badge">
-                            <span class="badge badge-danger">OFFER</span> <small>65% off | Use Coupon OSAHAN50</small>
-                         </div>
-                      </div>
-                   </div>
-                </div>
-                <div class="item">
-                   <div class="list-card bg-white h-100 rounded overflow-hidden position-relative shadow-sm">
-                      <div class="list-card-image">
-                         <div class="star position-absolute"><span class="badge badge-success"><i class="icofont-star"></i> 3.1 (300+)</span></div>
-                         <div class="favourite-heart text-danger position-absolute"><a href="detail.html"><i class="icofont-heart"></i></a></div>
-                         <div class="member-plan position-absolute"><span class="badge badge-dark">Promoted</span></div>
-                         <a href="detail.html">
-                         <img src="img/list/8.png" class="img-fluid item-img">
-                         </a>
-                      </div>
-                      <div class="p-3 position-relative">
-                         <div class="list-card-body">
-                            <h6 class="mb-1"><a href="detail.html" class="text-black">Polo Lounge
-                               </a>
-                            </h6>
-                            <p class="text-gray mb-3">North Indian • Indian • Pure veg</p>
-                            <p class="text-gray mb-3 time"><span class="bg-light text-dark rounded-sm pl-2 pb-1 pt-1 pr-2"><i class="icofont-wall-clock"></i> 15–25 min</span> <span class="float-right text-black-50"> $250 FOR TWO</span></p>
-                         </div>
-                         <div class="list-card-badge">
-                            <span class="badge badge-danger">OFFER</span> <small>65% off | Use Coupon OSAHAN50</small>
-                         </div>
-                      </div>
-                   </div>
-                </div>
-                <div class="item">
-                   <div class="list-card bg-white h-100 rounded overflow-hidden position-relative shadow-sm">
-                      <div class="list-card-image">
-                         <div class="star position-absolute"><span class="badge badge-success"><i class="icofont-star"></i> 3.1 (300+)</span></div>
-                         <div class="favourite-heart text-danger position-absolute"><a href="detail.html"><i class="icofont-heart"></i></a></div>
-                         <div class="member-plan position-absolute"><span class="badge badge-dark">Promoted</span></div>
-                         <a href="detail.html">
-                         <img src="img/list/9.png" class="img-fluid item-img">
-                         </a>
-                      </div>
-                      <div class="p-3 position-relative">
-                         <div class="list-card-body">
-                            <h6 class="mb-1"><a href="detail.html" class="text-black">Jack Fry's
-                               </a>
-                            </h6>
-                            <p class="text-gray mb-3">North Indian • Indian • Pure veg</p>
-                            <p class="text-gray mb-3 time"><span class="bg-light text-dark rounded-sm pl-2 pb-1 pt-1 pr-2"><i class="icofont-wall-clock"></i> 15–25 min</span> <span class="float-right text-black-50"> $250 FOR TWO</span></p>
-                         </div>
-                         <div class="list-card-badge">
-                            <span class="badge badge-danger">OFFER</span> <small>65% off | Use Coupon OSAHAN50</small>
-                         </div>
-                      </div>
-                   </div>
-                </div>
+                </div>  
+          </div> 
+          {{-- // end col md-3 --}}
+
+
+          <div class="col-md-3">  
+            <div class="item pb-3">
+               <div class="list-card bg-white h-100 rounded overflow-hidden position-relative shadow-sm">
+                  <div class="list-card-image">
+                     <div class="star position-absolute"><span class="badge badge-success"><i class="icofont-star"></i> 3.1 (300+)</span></div>
+                     <div class="favourite-heart text-danger position-absolute"><a href="detail.html"><i class="icofont-heart"></i></a></div>
+                     <div class="member-plan position-absolute"><span class="badge badge-dark">Promoted</span></div>
+                     <a href="detail.html">
+                     <img src="{{ asset('frontend/img/list/1.png') }}" class="img-fluid item-img">
+                     </a>
+                  </div>
+                  <div class="p-3 position-relative">
+                     <div class="list-card-body">
+                        <h6 class="mb-1"><a href="detail.html" class="text-black">World Famous</a></h6>
+                        <p class="text-gray mb-3">North Indian • American • Pure veg</p>
+                        <p class="text-gray mb-3 time"><span class="bg-light text-dark rounded-sm pl-2 pb-1 pt-1 pr-2"><i class="icofont-wall-clock"></i> 20–25 min</span> <span class="float-right text-black-50"> $250 FOR TWO</span></p>
+                     </div>
+                     <div class="list-card-badge">
+                        <span class="badge badge-success">OFFER</span> <small>65% off | Use Coupon OSAHAN50</small>
+                     </div>
+                  </div>
+               </div>
+            </div>  
+      </div> 
+      {{-- // end col md-3 --}}
+
+
+      <div class="col-md-3">  
+        <div class="item pb-3">
+           <div class="list-card bg-white h-100 rounded overflow-hidden position-relative shadow-sm">
+              <div class="list-card-image">
+                 <div class="star position-absolute"><span class="badge badge-success"><i class="icofont-star"></i> 3.1 (300+)</span></div>
+                 <div class="favourite-heart text-danger position-absolute"><a href="detail.html"><i class="icofont-heart"></i></a></div>
+                 <div class="member-plan position-absolute"><span class="badge badge-dark">Promoted</span></div>
+                 <a href="detail.html">
+                 <img src="{{ asset('frontend/img/list/1.png') }}" class="img-fluid item-img">
+                 </a>
+              </div>
+              <div class="p-3 position-relative">
+                 <div class="list-card-body">
+                    <h6 class="mb-1"><a href="detail.html" class="text-black">World Famous</a></h6>
+                    <p class="text-gray mb-3">North Indian • American • Pure veg</p>
+                    <p class="text-gray mb-3 time"><span class="bg-light text-dark rounded-sm pl-2 pb-1 pt-1 pr-2"><i class="icofont-wall-clock"></i> 20–25 min</span> <span class="float-right text-black-50"> $250 FOR TWO</span></p>
+                 </div>
+                 <div class="list-card-badge">
+                    <span class="badge badge-success">OFFER</span> <small>65% off | Use Coupon OSAHAN50</small>
+                 </div>
+              </div>
+           </div>
+        </div>  
+  </div> 
+  {{-- // end col md-3 --}}
+
+  <div class="col-md-3">  
+    <div class="item pb-3">
+       <div class="list-card bg-white h-100 rounded overflow-hidden position-relative shadow-sm">
+          <div class="list-card-image">
+             <div class="star position-absolute"><span class="badge badge-success"><i class="icofont-star"></i> 3.1 (300+)</span></div>
+             <div class="favourite-heart text-danger position-absolute"><a href="detail.html"><i class="icofont-heart"></i></a></div>
+             <div class="member-plan position-absolute"><span class="badge badge-dark">Promoted</span></div>
+             <a href="detail.html">
+             <img src="{{ asset('frontend/img/list/1.png') }}" class="img-fluid item-img">
+             </a>
+          </div>
+          <div class="p-3 position-relative">
+             <div class="list-card-body">
+                <h6 class="mb-1"><a href="detail.html" class="text-black">World Famous</a></h6>
+                <p class="text-gray mb-3">North Indian • American • Pure veg</p>
+                <p class="text-gray mb-3 time"><span class="bg-light text-dark rounded-sm pl-2 pb-1 pt-1 pr-2"><i class="icofont-wall-clock"></i> 20–25 min</span> <span class="float-right text-black-50"> $250 FOR TWO</span></p>
+             </div>
+             <div class="list-card-badge">
+                <span class="badge badge-success">OFFER</span> <small>65% off | Use Coupon OSAHAN50</small>
              </div>
           </div>
+       </div>
+    </div>  
+</div> 
+{{-- // end col md-3 --}}
+
+
+<div class="col-md-3">  
+    <div class="item pb-3">
+       <div class="list-card bg-white h-100 rounded overflow-hidden position-relative shadow-sm">
+          <div class="list-card-image">
+             <div class="star position-absolute"><span class="badge badge-success"><i class="icofont-star"></i> 3.1 (300+)</span></div>
+             <div class="favourite-heart text-danger position-absolute"><a href="detail.html"><i class="icofont-heart"></i></a></div>
+             <div class="member-plan position-absolute"><span class="badge badge-dark">Promoted</span></div>
+             <a href="detail.html">
+             <img src="{{ asset('frontend/img/list/1.png') }}" class="img-fluid item-img">
+             </a>
+          </div>
+          <div class="p-3 position-relative">
+             <div class="list-card-body">
+                <h6 class="mb-1"><a href="detail.html" class="text-black">World Famous</a></h6>
+                <p class="text-gray mb-3">North Indian • American • Pure veg</p>
+                <p class="text-gray mb-3 time"><span class="bg-light text-dark rounded-sm pl-2 pb-1 pt-1 pr-2"><i class="icofont-wall-clock"></i> 20–25 min</span> <span class="float-right text-black-50"> $250 FOR TWO</span></p>
+             </div>
+             <div class="list-card-badge">
+                <span class="badge badge-success">OFFER</span> <small>65% off | Use Coupon OSAHAN50</small>
+             </div>
+          </div>
+       </div>
+    </div>  
+</div> 
+{{-- // end col md-3 --}}
+
+
+<div class="col-md-3">  
+    <div class="item pb-3">
+       <div class="list-card bg-white h-100 rounded overflow-hidden position-relative shadow-sm">
+          <div class="list-card-image">
+             <div class="star position-absolute"><span class="badge badge-success"><i class="icofont-star"></i> 3.1 (300+)</span></div>
+             <div class="favourite-heart text-danger position-absolute"><a href="detail.html"><i class="icofont-heart"></i></a></div>
+             <div class="member-plan position-absolute"><span class="badge badge-dark">Promoted</span></div>
+             <a href="detail.html">
+             <img src="{{ asset('frontend/img/list/1.png') }}" class="img-fluid item-img">
+             </a>
+          </div>
+          <div class="p-3 position-relative">
+             <div class="list-card-body">
+                <h6 class="mb-1"><a href="detail.html" class="text-black">World Famous</a></h6>
+                <p class="text-gray mb-3">North Indian • American • Pure veg</p>
+                <p class="text-gray mb-3 time"><span class="bg-light text-dark rounded-sm pl-2 pb-1 pt-1 pr-2"><i class="icofont-wall-clock"></i> 20–25 min</span> <span class="float-right text-black-50"> $250 FOR TWO</span></p>
+             </div>
+             <div class="list-card-badge">
+                <span class="badge badge-success">OFFER</span> <small>65% off | Use Coupon OSAHAN50</small>
+             </div>
+          </div>
+       </div>
+    </div>  
+</div> 
+{{-- // end col md-3 --}}
+
+
+
+
        </div>
     </div>
  </section>
