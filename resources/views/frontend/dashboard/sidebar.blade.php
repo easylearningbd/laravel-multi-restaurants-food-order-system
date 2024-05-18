4a91566c80f85ce51c454995647384c51ad44d74
easylearningbd <elebdsoft@gmail.com>
2024-05-19 03:55:58 +0600

    43 User Dashboard Setup Part 2

---
+++ resources/views/frontend/dashboard/sidebar.blade.php
@@ -0,0 +1,38 @@
+<div class="col-md-3">
+    <div class="osahan-account-page-left shadow-sm rounded bg-white h-100">
+       <div class="border-bottom p-4">
+          <div class="osahan-user text-center">
+             <div class="osahan-user-media">
+                <img class="mb-3 rounded-pill shadow-sm mt-1" src="img/user/4.png" alt="gurdeep singh osahan">
+                <div class="osahan-user-media-body">
+                   <h6 class="mb-2">Gurdeep Singh</h6>
+                   <p class="mb-1">+91 85680-79956</p>
+                   <p>iamosahan@gmail.com</p>
+                   <p class="mb-0 text-black font-weight-bold"><a class="text-primary mr-3" data-toggle="modal" data-target="#edit-profile-modal" href="#"><i class="icofont-ui-edit"></i> EDIT</a></p>
+                </div>
+             </div>
+          </div>
+       </div>
+       <ul class="nav nav-tabs flex-column border-0 pt-4 pl-4 pb-4" id="myTab" role="tablist">
+        <li class="nav-item">
+            <a class="nav-link active"  href="{{ route('dashboard') }}" role="tab" aria-controls="orders" aria-selected="true"><i class="icofont-food-cart"></i> Profile</a>
+         </li>
+
+          <li class="nav-item">
+             <a class="nav-link" id="orders-tab" data-toggle="tab" href="#orders" role="tab" aria-controls="orders" aria-selected="true"><i class="icofont-food-cart"></i> Orders</a>
+          </li>
+          <li class="nav-item">
+             <a class="nav-link" id="offers-tab" data-toggle="tab" href="#offers" role="tab" aria-controls="offers" aria-selected="false"><i class="icofont-sale-discount"></i> Offers</a>
+          </li>
+          <li class="nav-item">
+             <a class="nav-link" id="favourites-tab" data-toggle="tab" href="#favourites" role="tab" aria-controls="favourites" aria-selected="false"><i class="icofont-heart"></i> Favourites</a>
+          </li>
+          <li class="nav-item">
+             <a class="nav-link" id="payments-tab" data-toggle="tab" href="#payments" role="tab" aria-controls="payments" aria-selected="false"><i class="icofont-credit-card"></i> Payments</a>
+          </li>
+          <li class="nav-item">
+             <a class="nav-link" id="addresses-tab" data-toggle="tab" href="#addresses" role="tab" aria-controls="addresses" aria-selected="false"><i class="icofont-location-pin"></i> Addresses</a>
+          </li>
+       </ul>
+    </div>
+ </div>
