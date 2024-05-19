f84c7000aa9ca4baecf0b7726e4f79409bf3b0e6
easylearningbd <elebdsoft@gmail.com>
2024-05-19 21:41:24 +0600

    52 Install Image Intervention Package

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth; 
 use App\Models\Category;
+use Intervention\Image\ImageManager;
+use Intervention\Image\Drivers\Gd\Driver;
 
 class CategoryController extends Controller
 {
