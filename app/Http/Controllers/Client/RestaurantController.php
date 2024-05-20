b16fae629950830f787a485f7c8bb5a556f1268f
easylearningbd <elebdsoft@gmail.com>
2024-05-20 20:11:51 +0600

    66 Restaurant Product Option Setup Part 5

--- app/Http/Controllers/Client/RestaurantController.php
+++ app/Http/Controllers/Client/RestaurantController.php
@@ -11,6 +11,8 @@ use Intervention\Image\Drivers\Gd\Driver;
 use App\Models\Menu;
 use App\Models\Product;
 use App\Models\City;
+use Haruncpi\LaravelIdGenerator\IdGenerator;
+use Carbon\Carbon;
 
 class RestaurantController extends Controller
 {
@@ -130,10 +132,47 @@ class RestaurantController extends Controller
     } 
     // End Method 
 
-    public function StoreProduct(){
+    public function StoreProduct(Request $request){
 
+        $pcode = IdGenerator::generate(['table' => 'products','field' => 'code', 'length' => 5, 'prefix' => 'PC']);  
+
+        if ($request->file('image')) {
+            $image = $request->file('image');
+            $manager = new ImageManager(new Driver());
+            $name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();
+            $img = $manager->read($image);
+            $img->resize(300,300)->save(public_path('upload/product/'.$name_gen));
+            $save_url = 'upload/product/'.$name_gen;
+
+            Product::create([
+                'name' => $request->name,
+                'slug' => strtolower(str_replace(' ','-',$request->name)),
+                'category_id' => $request->category_id,
+                'city_id' => $request->city_id,
+                'menu_id' => $request->menu_id,
+                'code' => $pcode,
+                'qty' => $request->qty,
+                'size' => $request->size,
+                'price' => $request->price,
+                'discount_price' => $request->discount_price,
+                'client_id' => Auth::guard('client')->id(),
+                'most_populer' => $request->most_populer,
+                'best_seller' => $request->best_seller,
+                'status' => 1,
+                'created_at' => Carbon::now(),
+                'image' => $save_url, 
+            ]); 
+        } 
+
+        $notification = array(
+            'message' => 'Product Inserted Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->route('all.product')->with($notification);
+                   
     }
-     // End Method 
+    // End Method 
 
 
 
