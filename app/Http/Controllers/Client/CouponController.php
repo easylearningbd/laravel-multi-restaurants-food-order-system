2df941e6b2133bedc17ee7f00d58001a296da675
easylearningbd <elebdsoft@gmail.com>
2024-05-30 20:08:01 +0600

    80 Fixed some Issue For Menu and Product  Part 2

--- app/Http/Controllers/Client/CouponController.php
+++ app/Http/Controllers/Client/CouponController.php
@@ -11,7 +11,8 @@ use Carbon\Carbon;
 class CouponController extends Controller
 {
     public function AllCoupon(){
-        $coupon = Coupon::latest()->get();
+        $cid = Auth::guard('client')->id();
+        $coupon = Coupon::where('client_id',$cid )->latest()->get();
         return view('client.backend.coupon.all_coupon', compact('coupon'));
     } 
     // End Method 
--- app/Http/Controllers/Client/RestaurantController.php
+++ app/Http/Controllers/Client/RestaurantController.php
@@ -180,10 +180,10 @@ class RestaurantController extends Controller
     // End Method 
 
     public function EditProduct($id){
-        $id = Auth::guard('client')->id();
+        $cid = Auth::guard('client')->id();
         $category = Category::latest()->get();
         $city = City::latest()->get();
-        $menu = Menu::where('client_id',$id)->latest()->get();
+        $menu = Menu::where('client_id',$cid)->latest()->get();
         $product = Product::find($id);
         return view('client.backend.product.edit_product', compact('category','city','menu','product'));
     } 
@@ -281,7 +281,8 @@ class RestaurantController extends Controller
      /////////// All Gallery Method Start 
 
      public function AllGallery(){
-        $gallery = Gllery::latest()->get();
+        $cid = Auth::guard('client')->id();
+        $gallery = Gllery::where('client_id',$cid)->latest()->get();
         return view('client.backend.gallery.all_gallery', compact('gallery'));
     } 
     // End Method 
