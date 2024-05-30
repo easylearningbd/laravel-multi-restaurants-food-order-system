6cfaa198b84d6d923799b712dabb9699cbbb9fd6
easylearningbd <elebdsoft@gmail.com>
2024-05-30 19:55:17 +0600

    79 Fixed some Issue For Menu and Product Part 1

--- app/Http/Controllers/Client/RestaurantController.php
+++ app/Http/Controllers/Client/RestaurantController.php
@@ -18,7 +18,8 @@ use App\Models\Gllery;
 class RestaurantController extends Controller
 {
     public function AllMenu(){
-        $menu = Menu::latest()->get();
+        $id = Auth::guard('client')->id();
+        $menu = Menu::where('client_id',$id)->orderBy('id','desc')->get();
         return view('client.backend.menu.all_menu', compact('menu'));
     } 
     // End Method 
@@ -41,6 +42,7 @@ class RestaurantController extends Controller
 
             Menu::create([
                 'menu_name' => $request->menu_name,
+                'client_id' => Auth::guard('client')->id(),
                 'image' => $save_url, 
             ]); 
         } 
@@ -120,15 +122,17 @@ class RestaurantController extends Controller
     ////// All Product Method started 
 
     public function AllProduct(){
-        $product = Product::latest()->get();
+        $id = Auth::guard('client')->id();
+        $product = Product::where('client_id',$id)->orderBy('id','desc')->get();
         return view('client.backend.product.all_product', compact('product'));
     } 
     // End Method 
 
     public function AddProduct(){
+        $id = Auth::guard('client')->id();
         $category = Category::latest()->get();
         $city = City::latest()->get();
-        $menu = Menu::latest()->get();
+        $menu = Menu::where('client_id',$id)->latest()->get();
         return view('client.backend.product.add_product', compact('category','city','menu'));
     } 
     // End Method 
@@ -176,9 +180,10 @@ class RestaurantController extends Controller
     // End Method 
 
     public function EditProduct($id){
+        $id = Auth::guard('client')->id();
         $category = Category::latest()->get();
         $city = City::latest()->get();
-        $menu = Menu::latest()->get();
+        $menu = Menu::where('client_id',$id)->latest()->get();
         $product = Product::find($id);
         return view('client.backend.product.edit_product', compact('category','city','menu','product'));
     } 
