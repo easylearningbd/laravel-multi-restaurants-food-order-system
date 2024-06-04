7329f83385a45bff4737898bacf8e90caec6c4c9
easylearningbd <elebdsoft@gmail.com>
2024-06-05 01:54:46 +0600

    113 Product Add in Cart with Price Part 3

--- resources/views/frontend/details_page.blade.php
+++ resources/views/frontend/details_page.blade.php
@@ -424,101 +424,44 @@ $coupons = App\Models\Coupon::where('client_id',$client->id)->where('status','1'
                 </div>
              </div>
              </div>
+             
              <div class="generator-bg rounded shadow-sm mb-4 p-4 osahan-cart-item">
                 <h5 class="mb-1 text-white">Your Order</h5>
-                <p class="mb-4 text-white">6 ITEMS</p>
-                <div class="bg-white rounded shadow-sm mb-2">
-                   <div class="gold-members p-2 border-bottom">
-                      <p class="text-gray mb-0 float-right ml-2">$314</p>
-                      <span class="count-number float-right">
-                      <button class="btn btn-outline-secondary  btn-sm left dec"> <i class="icofont-minus"></i> </button>
-                      <input class="count-number-input" type="text" value="1" readonly="">
-                      <button class="btn btn-outline-secondary btn-sm right inc"> <i class="icofont-plus"></i> </button>
-                      </span>
-                      <div class="media">
-                         <div class="mr-2"><i class="icofont-ui-press text-danger food-item"></i></div>
-                         <div class="media-body">
-                            <p class="mt-1 mb-0 text-black">Chicken Tikka Sub</p>
-                         </div>
-                      </div>
-                   </div>
-                   <div class="gold-members p-2 border-bottom">
-                      <p class="text-gray mb-0 float-right ml-2">$260</p>
-                      <span class="count-number float-right">
-                      <button class="btn btn-outline-secondary  btn-sm left dec"> <i class="icofont-minus"></i> </button>
-                      <input class="count-number-input" type="text" value="1" readonly="">
-                      <button class="btn btn-outline-secondary btn-sm right inc"> <i class="icofont-plus"></i> </button>
-                      </span>
-                      <div class="media">
-                         <div class="mr-2"><i class="icofont-ui-press text-success food-item"></i></div>
-                         <div class="media-body">
-                            <p class="mt-1 mb-0 text-black">Cheese corn Roll</p>
-                         </div>
-                      </div>
-                   </div>
-                   <div class="gold-members p-2 border-bottom">
-                      <p class="text-gray mb-0 float-right ml-2">$260</p>
-                      <span class="count-number float-right">
-                      <button class="btn btn-outline-secondary  btn-sm left dec"> <i class="icofont-minus"></i> </button>
-                      <input class="count-number-input" type="text" value="1" readonly="">
-                      <button class="btn btn-outline-secondary btn-sm right inc"> <i class="icofont-plus"></i> </button>
-                      </span>
-                      <div class="media">
-                         <div class="mr-2"><i class="icofont-ui-press text-success food-item"></i></div>
-                         <div class="media-body">
-                            <p class="mt-1 mb-0 text-black">Cheese corn Roll</p>
-                         </div>
-                      </div>
-                   </div>
-                   <div class="gold-members p-2 border-bottom">
-                      <p class="text-gray mb-0 float-right ml-2">$056</p>
-                      <span class="count-number float-right">
-                      <button class="btn btn-outline-secondary  btn-sm left dec"> <i class="icofont-minus"></i> </button>
-                      <input class="count-number-input" type="text" value="1" readonly="">
-                      <button class="btn btn-outline-secondary btn-sm right inc"> <i class="icofont-plus"></i> </button>
-                      </span>
-                      <div class="media">
-                         <div class="mr-2"><i class="icofont-ui-press text-success food-item"></i></div>
-                         <div class="media-body">
-                            <p class="mt-1 mb-0 text-black">Coke [330 ml]</p>
-                         </div>
-                      </div>
-                   </div>
-                   <div class="gold-members p-2 border-bottom">
-                      <p class="text-gray mb-0 float-right ml-2">$652</p>
-                      <span class="count-number float-right">
-                      <button class="btn btn-outline-secondary  btn-sm left dec"> <i class="icofont-minus"></i> </button>
-                      <input class="count-number-input" type="text" value="1" readonly="">
-                      <button class="btn btn-outline-secondary btn-sm right inc"> <i class="icofont-plus"></i> </button>
-                      </span>
-                      <div class="media">
-                         <div class="mr-2"><i class="icofont-ui-press text-danger food-item"></i></div>
-                         <div class="media-body">
-                            <p class="mt-1 mb-0 text-black">Black Dal Makhani</p>
-                         </div>
-                      </div>
-                   </div>
-                   <div class="gold-members p-2">
-                      <p class="text-gray mb-0 float-right ml-2">$122</p>
-                      <span class="count-number float-right">
-                      <button class="btn btn-outline-secondary  btn-sm left dec"> <i class="icofont-minus"></i> </button>
-                      <input class="count-number-input" type="text" value="1" readonly="">
-                      <button class="btn btn-outline-secondary btn-sm right inc"> <i class="icofont-plus"></i> </button>
-                      </span>
-                      <div class="media">
-                         <div class="mr-2"><i class="icofont-ui-press text-danger food-item"></i></div>
-                         <div class="media-body">
-                            <p class="mt-1 mb-0 text-black">Mixed Veg</p>
-                         </div>
-                      </div>
-                   </div>
+                <p class="mb-4 text-white">{{ count((array) session('cart')) }} ITEMS</p>
+    <div class="bg-white rounded shadow-sm mb-2">
+                   
+   @php $total = 0 @endphp
+   @if (session('cart'))
+      @foreach (session('cart') as $id => $details) 
+      @php
+         $total += $details['price'] * $details['quantity']
+      @endphp
+
+   <div class="gold-members p-2 border-bottom">
+         <p class="text-gray mb-0 float-right ml-2">${{ $details['price'] * $details['quantity'] }}</p>
+         <span class="count-number float-right">
+         <button class="btn btn-outline-secondary  btn-sm left dec"> <i class="icofont-minus"></i> </button>
+         <input class="count-number-input" type="text" value="1" readonly="">
+         <button class="btn btn-outline-secondary btn-sm right inc"> <i class="icofont-plus"></i> </button>
+         </span>
+         <div class="media">
+            <div class="mr-2"><img src="{{ asset($details['image']) }}"  width="25px" ></div>
+            <div class="media-body">
+               <p class="mt-1 mb-0 text-black">{{ $details['name'] }}</p>
+            </div>
+         </div>
+      </div>
+      @endforeach
+      @endif
+
+                   
                 </div>
-                <div class="mb-2 bg-white rounded p-2 clearfix">
-                   <img class="img-fluid float-left" src="img/wallet-icon.png">
-                   <h6 class="font-weight-bold text-right mb-2">Subtotal : <span class="text-danger">$456.4</span></h6>
-                   <p class="seven-color mb-1 text-right">Extra charges may apply</p>
-                   <p class="text-black mb-0 text-right">You have saved $955 on the bill</p>
-                </div>
+   <div class="mb-2 bg-white rounded p-2 clearfix">
+      <img class="img-fluid float-left" src="{{ asset('frontend/img/wallet-icon.png') }}">
+      <h6 class="font-weight-bold text-right mb-2">Subtotal : <span class="text-danger">${{ $total }}</span></h6>
+      <p class="seven-color mb-1 text-right">Extra charges may apply</p>
+      <p class="text-black mb-0 text-right">You have saved $955 on the bill</p>
+   </div>
                 <a href="checkout.html" class="btn btn-success btn-block btn-lg">Checkout <i class="icofont-long-arrow-right"></i></a>
              </div>
              
