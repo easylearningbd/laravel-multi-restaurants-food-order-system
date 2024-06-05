770961e0b024f2bbce3fd2e2c5f9f2ef76005da5
easylearningbd <elebdsoft@gmail.com>
2024-06-05 14:31:03 +0600

    120 Apply Coupon In Cart Page Part 3

--- app/Http/Controllers/Frontend/CartController.php
+++ app/Http/Controllers/Frontend/CartController.php
@@ -8,6 +8,7 @@ use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth; 
 use App\Models\Product;
 use Illuminate\Support\Facades\Session; 
+use App\Models\Coupon;
 
 class CartController extends Controller
 {
@@ -72,7 +73,47 @@ class CartController extends Controller
      }
       //End Method 
 
-    public function ApplyCoupon(){
+    public function ApplyCoupon(Request $request){
+        $coupon = Coupon::where('coupon_name',$request->coupon_name)->where('validit','>=',Carbon::now()->format('Y-m-d'))->first();
+
+        $cart = session()->get('cart',[]);
+        $totalAmount = 0;
+        $clientIds = [];
+
+        foreach($cart as $car){
+            $totalAmount += ($car['price'] * $car['quantity']);
+            $pd = Product::find($car['id']);
+            $cdid = $pd->client_id;
+            array_push($clientIds,$cdid);
+        }
+
+        if ($coupon) {
+           if (count(array_unique($clientIds)) === 1) {
+             $cvendorId = $coupon->client_id;
+
+             if ($cvendorId == $clientIds[0]) {
+                Session::put('coupon',[
+                    'coupon_name' => $coupon->coupon_name,
+                    'discount' => $coupon->discount,
+                    'discount_amount' => $totalAmount - ($totalAmount * $coupon->discount/100),
+                ]);
+                $couponData = Session()->get('coupon');
+
+                return response()->json(array(
+                    'validity' => true,
+                    'success' => 'Coupon Applied Successfully',
+                    'couponData' => $couponData,
+                ));
+             }else{
+                return response()->json(['error' => 'This Coupon Not Valid for this Restrurant']);
+             } 
+
+           }else{
+            return response()->json(['error' => 'This Coupon for one of the selected Restrurant']);
+           }
+        }else {
+            return response()->json(['error' => 'Invalid Coupon']);
+        }
 
     }
      //End Method 
