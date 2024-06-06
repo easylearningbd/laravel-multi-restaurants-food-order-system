4104d383e117282b219a5314dfc0fc2c9a030d1d
easylearningbd <elebdsoft@gmail.com>
2024-06-07 03:11:18 +0600

    140 Manage Order From Restaurant Panel Part 2

--- resources/views/client/backend/order/all_orders.blade.php
+++ resources/views/client/backend/order/all_orders.blade.php
@@ -1,10 +1,91 @@
 @extends('client.client_dashboard')
 @section('client')
 
+<div class="page-content">
+    <div class="container-fluid">
+
+        <!-- start page title -->
+        <div class="row">
+            <div class="col-12">
+                <div class="page-title-box d-sm-flex align-items-center justify-content-between">
+                    <h4 class="mb-sm-0 font-size-18">Client All Orders</h4>
+
+                    <div class="page-title-right">
+                        <ol class="breadcrumb m-0">
+                           
+                        </ol>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <!-- end page title -->
+
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                     
+                    <div class="card-body">
+
+        <table id="datatable" class="table table-bordered dt-responsive  nowrap w-100">
+            <thead>
+            <tr>
+                <th>Sl</th>
+                <th>Date</th>
+                <th>Invoice</th>
+                <th>Amount</th>
+                <th>Payment</th> 
+                <th>Status</th>
+                <th>Action </th> 
+            </tr>
+            </thead>
 
 
+            <tbody>
+           @foreach ($orderItemGroupData as $key=> $orderitem)
+           @php
+               $firstItem = $orderitem->first();
+               $order = $firstItem->order;
+           @endphp  
+            <tr>
+                <td>{{ $key+1 }}</td>
+                <td>{{ $order->order_date }}</td>
+                <td>{{ $order->invoice_no }}</td>
+                <td>${{ $order->amount }}</td>
+                <td>{{ $order->payment_method }}</td>
+                <td>
+                    @if ($order->status == 'Pending')
+                    <span class="badge bg-info">Pending</span>
+                    @elseif ($order->status == 'confirm')
+                    <span class="badge bg-primary">Confirm</span>
+                    @elseif ($order->status == 'processing')
+                    <span class="badge bg-warning">Processing</span>
+                    @elseif ($order->status == 'deliverd')
+                    <span class="badge bg-success">Deliverd</span>
+                    @endif
+                   </td>                
+               
+                
+        <td><a href="{{ route('admin.order.details',$order->id) }}" class="btn btn-info waves-effect waves-light"> <i class="fas fa-eye"></i> </a> 
 
+                </td> 
+            </tr>
+            @endforeach    
+            
+            </tbody>
+        </table>
 
+                    </div>
+                </div>
+            </div> <!-- end col -->
+        </div> <!-- end row --> 
+
+         
+    </div> <!-- container-fluid -->
+</div>
+
+ 
+   
 
 
 @endsection
