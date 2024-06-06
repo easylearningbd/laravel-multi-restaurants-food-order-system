a30bd8a0ede1240b956e0bef0c57a5244f3302d9
easylearningbd <elebdsoft@gmail.com>
2024-06-07 00:51:29 +0600

    136 Manage Order From Admin Panel Part 4

--- resources/views/admin/backend/order/admin_order_details.blade.php
+++ resources/views/admin/backend/order/admin_order_details.blade.php
@@ -21,18 +21,120 @@
         </div>
         <!-- end page title -->
 
-        <div class="row">
-            <div class="col-12">
-                <div class="card">
-                     
-                    <div class="card-body">
+<div class="row row-cols-1 row-cols-md-1 row-cols-lg-2 row-cols-xl-2">
+    
+    <div class="col">
+        <div class="card">
+         <div class="card-header">
+            <h4>Shipping Details</h4>
+         </div>
+                
+            <div class="card-body">
+<div class="table-responsive">
+    <table class="table table-bordered border-primary mb-0">
+ 
+        <tbody>
+            <tr> 
+                <th width="50%">Shipping Name: </th>
+                <td>{{ $order->name }}</td> 
+            </tr> 
+            <tr> 
+                <th width="50%">Shipping Phone: </th>
+                <td>{{ $order->phone }}</td> 
+            </tr>
+            <tr> 
+                <th width="50%">Shipping Email: </th>
+                <td>{{ $order->email }}</td> 
+            </tr>
+            <tr> 
+                <th width="50%">Shipping Address: </th>
+                <td>{{ $order->address }}</td> 
+            </tr>
+            <tr> 
+                <th width="50%">Order Date: </th>
+                <td>{{ $order->order_date }}</td> 
+            </tr> 
+            
+        </tbody>
+    </table>
+</div> 
+            </div>
+        </div>
+    </div> <!-- end col -->
 
-      
 
-                    </div>
-                </div>
-            </div> <!-- end col -->
-        </div> <!-- end row --> 
+    <div class="col">
+        <div class="card">
+         <div class="card-header">
+            <h4>Order Details
+            <span class="text-danger">Invoice: {{ $order->invoice_no }}</span></h4>
+         </div>
+                
+            <div class="card-body">
+<div class="table-responsive">
+    <table class="table table-bordered border-primary mb-0">
+ 
+        <tbody>
+            <tr> 
+                <th width="50%"> Name: </th>
+                <td>{{ $order->user->name }}</td> 
+            </tr> 
+            <tr> 
+                <th width="50%"> Phone: </th>
+                <td>{{ $order->user->phone }}</td> 
+            </tr>
+            <tr> 
+                <th width="50%"> Email: </th>
+                <td>{{ $order->user->email }}</td> 
+            </tr>
+            <tr> 
+                <th width="50%">Payment Type: </th>
+                <td>{{ $order->payment_method }}</td> 
+            </tr>
+            <tr> 
+                <th width="50%">Transx Id: </th>
+                <td>{{ $order->transaction_id }}</td> 
+            </tr> 
+            <tr> 
+                <th width="50%">Invoice: </th>
+                <td class="text-danger">{{ $order->invoice_no }}</td> 
+            </tr> 
+            <tr> 
+                <th width="50%">Order Amount: </th>
+                <td>${{ $order->amount }}</td> 
+            </tr> 
+            <tr> 
+                <th width="50%">Order Status: </th>
+                <td><span class="badge bg-success">{{ $order->status }}</span></td> 
+            </tr>
+
+            <tr> 
+                <th width="50%"> </th>
+                <td> 
+                    @if($order->status == 'Pending')
+                    <a href="" class="btn btn-block btn-success">Confirm Order</a>
+                    @elseif ($order->status == 'confirm')
+                    <a href="" class="btn btn-block btn-success">Processing Order</a>
+                    @elseif ($order->status == 'processing')
+                    <a href="" class="btn btn-block btn-success">Deliverd Order</a>
+                    @endif
+                </td> 
+            </tr>
+            
+        </tbody>
+    </table>
+</div> 
+            </div>
+        </div>
+    </div> <!-- end col -->
+
+
+
+
+
+
+
+</div> <!-- end row --> 
 
          
     </div> <!-- container-fluid -->
