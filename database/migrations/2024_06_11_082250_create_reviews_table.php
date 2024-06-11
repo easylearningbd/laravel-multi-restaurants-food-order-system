1ea125cbcfabb1f43559e48cdfe3dfa3d02e5bf1
easylearningbd <elebdsoft@gmail.com>
2024-06-11 14:33:47 +0600

    154 Restaurant Review System Part 1

---
+++ database/migrations/2024_06_11_082250_create_reviews_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('client_id')->unsigned();
+            $table->unsignedBigInteger('user_id')->unsigned();
+            $table->text('comment')->nullable();
+            $table->string('rating')->nullable();
+            $table->foreign('client_id')->references('id')->on('clients')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('status')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
